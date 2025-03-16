73d60c3fd36f0571947087e86f9463e6d55c0f43
Aarzu Patel <aarzupatel1105@gmail.com>
2025-03-16 13:18:04 -0400

    Update ProductPage.jsx

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -54,4 +54,46 @@ const ProductPage = () => {
               <div className="text-xl xl:text-2xl text-red-700 text-right font-semibold">
                 {GB_CURRENCY.format(product.price)}
               </div>
-              <div className="text-base xl:text-lg text-gray-500 text-right
+              <div className="text-base xl:text-lg text-gray-500 text-right font-semibold">
+                RRP:{" "}
+                <span className="line-through">
+                  {GB_CURRENCY.format(product.oldPrice)}
+                </span>
+              </div>
+              <div className="text-sm xl:text-base text-blue-500 font-semibold mt-3">
+                FREE Returns
+              </div>
+              <div className="text-sm xl:text-base text-blue-500 font-semibold mt-1">
+                FREE Delivery
+              </div>
+              <div className="text-base xl:text-lg text-green-700 font-semibold mt-1">
+                In Stock
+              </div>
+              <div className="text-base xl:text-lg mt-1">
+                Quantity:
+                <select
+                  onChange={(e) => setQuantity(e.target.value)}
+                  className="p-2 bg-white border rounded-md focus:border-indigo-600"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+              </div>
+              <Link to={"/checkout"}>
+                <button
+                  onClick={() => dispatch(addToCart(addQuantityToProduct()))}
+                  className="btn"
+                >
+                  Add to Cart
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default ProductPage;
